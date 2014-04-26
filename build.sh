d563e8f09cf82006e07d2a6ea12ad36f342ab7d6
Ian Tuomi <ian@Ians-MacBook-Air.local>
2014-04-26 23:34:25 +0300

    add <>, +++, <<< and >>>

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ mkdir target/ target/OTF/ target/TTF/
 
 for w in $weights
 do
-  makeotf -f Roman/$w/font.pfa -r -o target/OTF/$family-$w.otf
+  makeotf -f Roman/$w/font.pfa -r -o target/$family-$w.otf
 #  makeotf -f Roman/$w/font.ttf -gf GlyphOrderAndAliasDB_TT -r -o target/TTF/$family-$w.ttf
   rm Roman/$w/current.fpr # remove default options file from the source tree after building
 done
---
+++ buildAll.sh
@@ -0,0 +1,1 @@
+cd RomanMM;makeInstances -o ../Roman;cd ..;./build.sh
