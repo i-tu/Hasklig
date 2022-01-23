50c9b1ae8e643705a406b56f41118a2d04dc74f7
Oliver Kopp <kopp.dev@gmail.com>
2022-01-23 21:04:19 +0100

    Fix typo

--- makeInstances.sh
+++ makeInstances.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# We're just testing if 'makeInstancesUFO' exists, otherwise we'll attempt to use the lowercase varient.
+# We're just testing if 'makeInstancesUFO' exists, otherwise we'll attempt to use the lowercase variant.
 function makeInstancesUFO() { 
   [[ -z "$(which makeInstancesUFO)" ]] \
     && makeinstancesufo $argv || makeInstancesUFO $argv
